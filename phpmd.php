491faee00ddd121bc9ece5fa40882fec17b43a80
Manuel Pichler <mapi@manuel-pichler.de>
2009-01-20 20:12:08 +0000

    - Stream api based writer added.
    git-svn-id: svn://xplib.de/PHP_PMD/trunk@26 0bc4ca42-642f-4eb8-9a60-dc4da71a2dd1

--- phpmd.php
+++ phpmd.php
@@ -3,12 +3,15 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
 
 require_once 'PHP/Depend.php';
+require_once 'PHP/Depend/Input/ExcludePathFilter.php';
+require_once 'PHP/Depend/Input/ExtensionFilter.php';
+
 require_once 'PHP/PMD.php';
-require_once 'PHP/PMD/AbstractWriter.php';
 require_once 'PHP/PMD/Report.php';
 require_once 'PHP/PMD/RuleSetFactory.php';
 require_once 'PHP/PMD/Adapter/Metrics.php';
 require_once 'PHP/PMD/Renderer/XMLRenderer.php';
+require_once 'PHP/PMD/Writer/Stream.php';
 
 class MyWriter extends PHP_PMD_AbstractWriter {
     public function write($data) {
@@ -30,11 +33,13 @@ foreach ($ruleSets as $ruleSet) {
 
 $pdepend = new PHP_Depend();
 $pdepend->addDirectory(realpath($argv[1]));
+$pdepend->addFileFilter(new PHP_Depend_Input_ExcludePathFilter(array('.git', '.svn', 'CVS')));
+$pdepend->addFileFilter(new PHP_Depend_Input_ExtensionFilter(array('php', 'php3', 'php4', 'php5', 'inc')));
 $pdepend->addLogger($adapter);
 $pdepend->analyze();
 
 $renderer = new PHP_PMD_Renderer_XMLRenderer();
-$renderer->setWriter(new MyWriter());
+$renderer->setWriter(new PHP_PMD_Writer_Stream(STDOUT));
 $renderer->start();
 $renderer->renderReport($report);
 $renderer->end();
