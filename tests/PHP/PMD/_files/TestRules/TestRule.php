dfc2b521a8e4f672a65d973965849cbbd06fe2b9
Manuel Pichler <mapi@manuel-pichler.de>
2009-01-23 20:44:42 +0000

    - Test files added
    git-svn-id: svn://xplib.de/PHP_PMD/trunk@44 0bc4ca42-642f-4eb8-9a60-dc4da71a2dd1

---
+++ tests/PHP/PMD/_files/TestRules/TestRule.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'PHP/PMD/AbstractRule.php';
+require_once 'PHP/PMD/Rule/IClassAware.php';
+
+/**
+ * Simple text rule implementation.
+ */
+class TestRules_TestRule 
+       extends PHP_PMD_AbstractRule
+    implements PHP_PMD_Rule_IClassAware
+{
+    public $node = null;
+
+    public function apply(PHP_PMD_AbstractNode $node)
+    {
+        $this->node = $node;
+    }
+}
+?>
