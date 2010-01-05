093bacf0b95ac0d30f4c19674d8964814a609b55
Manuel Pichler <mapi@manuel-pichler.de>
2010-01-05 20:16:14 +0000

    # Author e-mail address changed.
    git-svn-id: svn://xplib.de/PHP_PMD/branches/0.2.x@169 0bc4ca42-642f-4eb8-9a60-dc4da71a2dd1

--- source/PHP/PMD/Parser.php
+++ source/PHP/PMD/Parser.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 5
  *
- * Copyright (c) 2009, Manuel Pichler <mapi@pdepend.org>.
+ * Copyright (c) 2009, Manuel Pichler <mapi@phpmd.org>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,7 +38,7 @@
  *
  * @category  PHP
  * @package   PHP_PMD
- * @author    Manuel Pichler <mapi@pdepend.org>
+ * @author    Manuel Pichler <mapi@phpmd.org>
  * @copyright 2009 Manuel Pichler. All rights reserved.
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   SVN: $Id$
@@ -59,7 +59,7 @@ require_once 'PHP/PMD/Node/Method.php';
  *
  * @category  PHP
  * @package   PHP_PMD
- * @author    Manuel Pichler <mapi@pdepend.org>
+ * @author    Manuel Pichler <mapi@phpmd.org>
  * @copyright 2009 Manuel Pichler. All rights reserved.
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   Release: @package_version@
--- test/PHP/PMD/Rule/UnusedFormalParameterTest.php
+++ test/PHP/PMD/Rule/UnusedFormalParameterTest.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 5
  *
- * Copyright (c) 2009, Manuel Pichler <mapi@pdepend.org>.
+ * Copyright (c) 2009, Manuel Pichler <mapi@phpmd.org>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,7 @@
  * @category   PHP
  * @package    PHP_PMD
  * @subpackage Rule
- * @author     Manuel Pichler <mapi@pdepend.org>
+ * @author     Manuel Pichler <mapi@phpmd.org>
  * @copyright  2009 Manuel Pichler. All rights reserved.
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version    SVN: $Id$
@@ -56,7 +56,7 @@ require_once 'PHP/PMD/Rule/UnusedFormalParameter.php';
  * @category   PHP
  * @package    PHP_PMD
  * @subpackage Rule
- * @author     Manuel Pichler <mapi@pdepend.org>
+ * @author     Manuel Pichler <mapi@phpmd.org>
  * @copyright  2009 Manuel Pichler. All rights reserved.
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version    Release: @package_version@
