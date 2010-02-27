477ad1af97a8b2511b56dd14ca87acd36d46f554
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-27 15:46:27 +0100

    # New rule that tests for deprecated php 4 constructor methods.

---
+++ test/PHP/PMD/_files/Rule/Naming/ConstructorWithNameAsEnclosingClass/testRuleAppliesToConstructorMethodNamedAsEnclosingClass.php
@@ -0,0 +1,8 @@
+<?php
+class testRuleAppliesToConstructorMethodNamedAsEnclosingClass
+{
+    function testRuleAppliesToConstructorMethodNamedAsEnclosingClass()
+    {
+        
+    }
+}
---
+++ test/PHP/PMD/_files/Rule/Naming/ConstructorWithNameAsEnclosingClass/testRuleAppliesToConstructorMethodNamedAsEnclosingClassCaseInsensitive.php
@@ -0,0 +1,8 @@
+<?php
+class testruleappliestoconstructormethodnamedasenclosingclasscaseinsensitive
+{
+    function testRuleAppliesToConstructorMethodNamedAsEnclosingClassCaseInsensitive()
+    {
+        
+    }
+}
