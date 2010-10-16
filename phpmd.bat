2317d821e618c938d33efd4dda148cc5601cda4b
Manuel Pichler <mapi@manuel-pichler.de>
2010-10-16 11:47:59 +0000

    # Batch file fixed, based on commit PHPUnit commit 32b7c5d813b6401d0e18.
    git-svn-id: svn://xplib.de/PHP_PMD/trunk@293 0bc4ca42-642f-4eb8-9a60-dc4da71a2dd1

--- phpmd.bat
+++ phpmd.bat
@@ -36,5 +36,10 @@ REM
 REM $Id$
 REM
 
-set PHPBIN="@php_bin@"
-"@php_bin@" "@bin_dir@\phpmd" %*
+if "%PHPBIN%" == "" set PHPBIN=@php_bin@
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "@bin_dir@\phpmd" %*
