f170d8cfcabfb54f003786a2e64eec5f66d0bbd2
jeroendedauw <jeroendedauw@gmail.com>
2015-09-25 19:05:47 +0200

    Add TestMethodName rule
    Fixes https://github.com/phpmd/phpmd/issues/251

---
+++ src/test/resources/files/Rule/Controversial/TestMethodName/testRuleAddsViolationForInvalidTestMethodNameTest.php
@@ -0,0 +1,8 @@
+<?php
+class testRuleAddsViolationForInvalidTestMethodNameTest
+{
+    public function testGivenFoobar_kittens()
+    {
+
+    }
+}
---
+++ src/test/resources/files/Rule/Controversial/TestMethodName/testRuleDoesNotAddViolationForNonTestMethodNameTest.php
@@ -0,0 +1,8 @@
+<?php
+class testRuleDoesNotAddViolationForNonTestMethodNameTest
+{
+    public function thisIsNotATestMethod()
+    {
+
+    }
+}
