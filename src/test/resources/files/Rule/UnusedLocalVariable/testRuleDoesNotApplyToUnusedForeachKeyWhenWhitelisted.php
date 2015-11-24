c4e270638105e85000e43db824824b36eee83ba8
Julien Palard <julien.palard@meltygroup.com>
2015-11-24 14:49:09 +0100

    Add unit tests for the UnusedLocalVariable exceptions parameter.

---
+++ src/test/resources/files/Rule/UnusedLocalVariable/testRuleDoesNotApplyToUnusedForeachKeyWhenWhitelisted.php
@@ -0,0 +1,10 @@
+<?php
+class testRuleDoesNotApplyToUnusedForeachKeyWhenWhitelisted
+{
+    public function testRuleDoesNotApplyToUnusedForeachKeyWhenWhitelisted()
+    {
+        foreach ($this->index as $_ => $value) {
+            self::$string{$value} = 'a';
+        }
+    }
+}
---
+++ src/test/resources/files/Rule/UnusedLocalVariable/testRuleStillAppliesWhenSomeUnusedLocaleAreWhitelisted.php
@@ -0,0 +1,8 @@
+<?php
+class testRuleStillAppliesWhenSomeUnusedLocaleAreWhitelisted
+{
+    function testRuleStillAppliesWhenSomeUnusedLocaleAreWhitelisted()
+    {
+        list($_, $y) = [42, 43];
+    }
+}
