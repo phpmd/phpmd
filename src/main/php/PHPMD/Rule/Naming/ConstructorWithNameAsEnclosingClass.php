072b239efca972db62bb6587dcf4716bb1d5af85
Marc Würth <ravage@bluewin.ch>
2017-06-02 20:58:13 +0200

    Replace all file header doc blocks with uniform one
    - Replace license text with reference to LICENSE file
    - Remove year notes in copyright
    - Add link to http://phpmd.org/
    
    Resolves  #206

--- src/main/php/PHPMD/Rule/Naming/ConstructorWithNameAsEnclosingClass.php
+++ src/main/php/PHPMD/Rule/Naming/ConstructorWithNameAsEnclosingClass.php
@@ -2,41 +2,17 @@
 /**
  * This file is part of PHP Mess Detector.
  *
- * Copyright (c) 2008-2017, Manuel Pichler <mapi@phpmd.org>.
+ * Copyright (c) Manuel Pichler <mapi@phpmd.org>.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *
- *   * Neither the name of Manuel Pichler nor the names of his
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Licensed under BSD License
+ * For full copyright and license information, please see the LICENSE file.
+ * Redistributions of files must retain the above copyright notice.
  *
  * @author Manuel Pichler <mapi@phpmd.org>
- * @copyright 2008-2017 Manuel Pichler. All rights reserved.
- * @license    https://opensource.org/licenses/bsd-license.php BSD License
+ * @copyright Manuel Pichler. All rights reserved.
+ * @license https://www.opensource.org/licenses/bsd-license.php BSD License
+ * @link http://phpmd.org/
  */
 
 namespace PHPMD\Rule\Naming;
--- src/test/resources/files/classes/does_not_follow_psr0.class.php
+++ src/test/resources/files/classes/does_not_follow_psr0.class.php
@@ -1,13 +1,27 @@
 <?php
 /**
- * @author Gerrit Addiks <gerrit@addiks.de>
+ * This file is part of PHP Mess Detector.
+ *
+ * Copyright (c) Manuel Pichler <mapi@phpmd.org>.
+ * All rights reserved.
+ *
+ * Licensed under BSD License
+ * For full copyright and license information, please see the LICENSE file.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author Manuel Pichler <mapi@phpmd.org>
+ * @copyright Manuel Pichler. All rights reserved.
+ * @license https://www.opensource.org/licenses/bsd-license.php BSD License
+ * @link http://phpmd.org/
  */
 
 /**
  * Some class that stands as an example for classes not following PSR-0.
+
+ * @author Gerrit Addiks <gerrit@addiks.de>
  */
 class some_class_that_does_not_follow_psr0 extends \PHPMD\AbstractRule{
-    
+
     /**
      * A method that returnes foo, bar and baz.
      *
@@ -16,9 +30,9 @@ class some_class_that_does_not_follow_psr0 extends \PHPMD\AbstractRule{
     public function getFooBarBaz(){
         return array('foo', 'bar', 'baz');
     }
-    
+
     public function apply(\PHPMD\AbstractNode $node){
-        
+
     }
-    
+
 }
