42d9be3549489edb35b1da736f6e678d457c3d93
Smuuf <pvspam@centrum.cz>
2017-11-19 14:47:53 +0100

    Reworked the new HTML renderer. - Report's sub title contains more information about report and machine. - New "summary" section with summaries calculated across:  - Problem's priority  - PHP namespace  - Rule set  - Rule name - Different design. - File paths act as links. - Deep linking possible, problem index acts as an anchor. - Details are hidden by default - can be toggled. - Attempt at printer friendly CSS media-queries.
    (Based on discussion at https://github.com/phpmd/phpmd/pull/529)

--- src/main/php/PHPMD/Renderer/PrettyHTMLRenderer.php
+++ src/main/php/PHPMD/Renderer/PrettyHTMLRenderer.php
@@ -41,6 +41,7 @@
 
 namespace PHPMD\Renderer;
 
+use PHPMD\PHPMD;
 use PHPMD\AbstractRenderer;
 use PHPMD\Report;
 
@@ -55,22 +56,35 @@ use PHPMD\Report;
 class PrettyHTMLRenderer extends AbstractRenderer
 {
 
+	const CATEGORY_PRIORITY = "category_priority";
+	const CATEGORY_NAMESPACE = "category_namespace";
+	const CATEGORY_RULESET = "category_ruleset";
+	const CATEGORY_RULE = "category_rule";
+
+	const PRIORITY_TITLES = [
+		1 => "Top (1)",
+		2 => "High (2)",
+		3 => "Moderate (3)",
+		4 => "Low (4)",
+		5 => "Lowest (5)",
+	];
+
 	// Used in self::colorize() method.
 	const INFO_HIGHLIGHTING = [
+		'method' => [
+			// Method names.
+			'regex' => 'method\s+(((["\']).*["\'])|(\S+))',
+			'css-class' => 'hlt-method',
+		],
 		'quoted' => [
 			// Quoted strings.
 			'regex' => '(["\'][^\'"]+["\'])',
-			'css-class' => 'highlight-quoted',
+			'css-class' => 'hlt-quoted',
 		],
 		'variable' => [
 			// Variables.
 			'regex' => '(\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)',
-			'css-class' => 'highlight-variable',
-		],
-		'method' => [
-			// Method names.
-			'regex' => '(method [a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)(\(.*\))?',
-			'css-class' => 'highlight-method',
+			'css-class' => 'hlt-variable',
 		],
 	];
 
@@ -86,46 +100,139 @@ class PrettyHTMLRenderer extends AbstractRenderer
 	{
 		$writer = $this->getWriter();
 
-		$brSize = "0.5ex";
-		$borderRadius = "border-radius: $brSize;";
-		$borderTopRadius = "border-top-left-radius: $brSize; border-top-right-radius: $brSize;";
-		$borderBottomRadius = "border-bottom-left-radius: $brSize; border-bottom-right-radius: $brSize;";
+		$mainColor = "#2f838a";
 
 		// Avoid inlining styles.
 		$style = "
+			<script>
+				function toggle(id) {
+					var item = document.getElementById(id);
+					item.classList.toggle('hidden');
+				}
+			</script>
 			<style>
+
+				@media (min-width: 1366px) {
+					body { max-width: 80%; margin: auto; }
+				}
 
 				body {
 					font-family: sans-serif;
 				}
 
-				.excerpt {
-					padding: 1ex;
-					background: #fff;
-					$borderBottomRadius
+				a {
+					color: $mainColor;
 				}
 
-				.code-line {
+				a:hover {
+					color: #333;
+				}
+
+				em {
+					font-weight: bold;
+					font-style: italic;
+				}
+
+				h1 {
+					padding: 0.5ex 0.2ex;
+					border-bottom: 2px solid #333;
+				}
+
+				table {
+					width: 100%;
+					border-spacing: 0;
+				}
+
+				table tr > th {
+					text-align: left;
+				}
+
+				table caption {
+					font-weight: bold;
+					padding: 1ex 0.5ex;
+					text-align: left;
+					font-size: 120%;
+					border-bottom: 2px solid #333;
+				}
+
+				tbody tr:nth-child(odd) {
+					background: rgba(0, 0, 0, 0.08);
+				}
+
+				tbody tr:hover {
+					background: #ffee99;
+				}
+
+				thead th {
+					border-bottom: 1px solid #aaa;
+				}
+
+				table td, table th {
+					padding: 0.5ex;
+				}
+
+				/* Table 'count' and 'percentage' column */
+				.t-cnt, .t-pct {
+					width: 5em;
+				}
+
+				.t-pct {
+					opacity: 0.8;
+					font-style: italic;
+					font-size: 80%;
+				}
+
+				/* Table bar chart */
+				.t-bar {
+					height: 0.5ex;
+					margin-top: 0.5ex;
+					background-color: $mainColor; /* rgba(47, 131, 138, 0.2); */
+				}
+
+				section, table {
+					margin-bottom: 2em;
+				}
+
+				#details-link.hidden {
+					display: none;
+				}
+
+				#details-wrapper.hidden {
+					display: none;
+				}
+
+				ul.code {
+					margin: 0;
+					padding: 0;
+				}
+
+				ul.code.hidden {
+					display: none;
+				}
+
+				ul.code li {
 					display: flex;
-					line-height: 1.2em;
+					line-height: 1.4em;
 					font-family: monospace;
 					white-space: nowrap;
 				}
 
-				.code-line:nth-child(2n + 1) {
-					background-color: rgba(175, 195, 202, 0.2)
+				ul.code li:nth-child(odd) {
+					background-color: rgba(47, 131, 138, 0.1)
 				}
 
-				.line-number {
+				/* Excerpt: Line number */
+				ul.code .no {
 					width: 5%;
 					min-width: 5em;
 					text-align: right;
-					border-right: 2px solid rgba(0, 0, 0, 0.2);
+					border-right: 1px solid rgba(47, 131, 138, 0.6);
 					padding-right: 1ex;
 					box-sizing: border-box;
 				}
 
-				.line-content {
+				/* Excerpt: Code */
+				ul.code .cd {
 					padding-left: 1ex;
 					white-space: pre-wrap;
 					box-sizing: border-box;
@@ -133,88 +240,96 @@ class PrettyHTMLRenderer extends AbstractRenderer
 					overflow: hidden;
 				}
 
-				.line-highlight {
+				.hlt {
 					background: #ffee99 !important
 				}
 
-				.line-highlight .line-number {
-					background-color: rgba(250, 150, 0, 0.4);
-					font-weight: bold;
+				.indx {
+					padding: 0.5ex 1ex;
+					background-color: #000;
+					color: #fff;
+					text-decoration: none;
 				}
 
-				.problem {
-					background: #afc3ca;
-					padding: 1ex;
-					margin-bottom: 2em;
-					$borderRadius
+				.indx:hover {
+					background-color: $mainColor;
+					color: #fff;
 				}
 
-				.problem > header {
-					display: flex;
-					margin-bottom: 1ex;
-					font-weight: bold;
-					font-size: 0.9rem;
+				/* Problem container */
+				.prb h3 {
+					padding: 1ex 0.5ex;
+					border-bottom: 2px solid #000;
+					font-size: 95%;
+					margin: 0;
 				}
 
-				.problem-info {
-					background: rgba(255, 255, 255, 0.75);
-					padding: 1ex;
-					flex: 1;
-					$borderRadius
+				.info-lnk {
+					font-style: italic !important;
+					font-weight: normal !important;
+					text-decoration: none;
 				}
 
-				.problem-index {
-					display: flex;
-					width: 3em;
-					background: rgba(255, 255, 255, 0.5);
-					padding: 1ex;
-					text-align: center;
-					justify-content: center;
-					flex-direction: column;
-					margin: 0 1ex 1px 0;
-					$borderRadius
+				.info-lnk.blck {
+					padding: 0.5ex 1ex;
+					background-color: rgba(47, 131, 138, 0.2);
 				}
 
 				.path-basename {
 					font-weight: bold;
 				}
 
-				.highlight-info {
+				.hlt-info {
 					display: inline-block;
-					$borderRadius
 					padding: 2px 4px;
 					font-style: italic;
 				}
 
-					.highlight-info.quoted {
+					.hlt-info.quoted {
 						background-color: #92de71;
 					}
 
-					.highlight-info.variable {
+					.hlt-info.variable {
 						background-color: #a3d2ff;
 					}
 
-					.highlight-info.method {
+					.hlt-info.method {
 						background-color: #f7c0ff;
 					}
 
-				.problem-file {
-					padding: 1ex;
-					background: #fbfbc0;
-					font-size: 0.8rem;
-					$borderTopRadius
+				.file-pth {
+					padding: 1ex 0.5ex;
 				}
 
-				.problem-link {
-					font-style: italic;
+				/* Handle printer friendly styles */
+				@media print {
+					body, th { font-size: 10pt; }
+					.hlt-info { padding: 0; background: none; }
+					section, table { margin-bottom: 1em; }
+					h1, h2, h3, table caption { padding: 0.5ex 0.2ex; }
+					.prb h3 { border-bottom: 0.5px solid #aaa; }
+					.t-bar { display: none; }
+					.info-lnk { display: none; }
+					#details-wrapper { display: block !important; font-size: 90% !important; }
 				}
 
-			</style>
-		";
+			</style>";
 
-		$writer->write("<html><head>{$style}<title>PHPMD Report</title></head><body>");
-		$writer->write(PHP_EOL);
-		$writer->write('<h1>PHPMD report</h1>');
+		$style = self::reduceWhitespace($style);
+		$writer->write("<html><head>{$style}<title>PHPMD Report</title></head><body>" . PHP_EOL);
+
+		$header = sprintf("
+			<header>
+				<h1>PHPMD Report</h1>
+				Generated at <em>%s</em>
+				with <a href='%s' target='_blank'>PHP Mess Detector</a>
+				on <em>PHP %s</em>
+				on <em>%s</em>
+			</header>
+		", date('Y-m-d H:i'), "https://phpmd.org", \PHP_VERSION, gethostname());
+
+		$writer->write($header);
+
 	}
 
 	/**
@@ -231,12 +346,39 @@ class PrettyHTMLRenderer extends AbstractRenderer
 		$index = 0;
 		$violations = $report->getRuleViolations();
 
-		$w->write(sprintf('<h2>%d problems found</h2>', count($violations)));
-		$w->write(PHP_EOL);
+		$count = count($violations);
+		$w->write(sprintf('<h3>%d problems found</h3>', $count));
+
+		// If no problems were found, don't bother with rendering anything else.
+		if (!$count) {
+			return;
+		}
+
+		// Render summary tables.
+		$w->write("<h2>Summary</h2>");
+		$categorized = self::sumUpViolations($violations);
+		$this->writeTable('By priority', 'Priority', $categorized[self::CATEGORY_PRIORITY]);
+		$this->writeTable('By namespace', 'PHP Namespace', $categorized[self::CATEGORY_NAMESPACE]);
+		$this->writeTable('By rule set', 'Rule set', $categorized[self::CATEGORY_RULESET]);
+		$this->writeTable('By name', 'Rule name', $categorized[self::CATEGORY_RULE]);
+
+		// Render details of each violation and place the "Details" display toggle.
+		$w->write("<h2 style='page-break-before: always'>Details</h2>");
+		$w->write("
+			<a
+				id='details-link'
+				class='info-lnk blck'
+				href='#'
+				onclick='toggle(\"details-link\"); toggle(\"details-wrapper\"); return false;'
+			>
+			Show details &#x25BC;
+		</a>");
+		$w->write("<div id='details-wrapper' class='hidden'>");
 
 		foreach ($violations as $violation) {
 
-			$index++;
+			// This is going to be used as ID in HTML (deep anchoring).
+			$htmlId = "p-" . $index++;
 
 			// Get excerpt of the code from validated file.
 			$excerptHtml = null;
@@ -247,40 +389,45 @@ class PrettyHTMLRenderer extends AbstractRenderer
 			);
 
 			foreach ($excerpt as $line => $code) {
-				$class = $line === $violation->getBeginLine() ? ' line-highlight' : null;
+				$class = $line === $violation->getBeginLine() ? " class='hlt'" : null;
 				$codeHtml = htmlspecialchars($code);
-				$excerptHtml .= "
-					<div class='code-line{$class}'>
-						<div class='line-number'>{$line}</div>
-						<div class='line-content'>{$codeHtml}</div>
-					</div>
-				";
+				$excerptHtml .= "<li{$class}><div class='no'>{$line}</div><div class='cd'>{$codeHtml}</div></li>";
 			}
 
-			$infoHtml = self::colorize(htmlentities($violation->getDescription()));
-			$fileHtml = self::highlightFile($violation->getFileName());
+			$descHtml = self::colorize(htmlentities($violation->getDescription()));
+			$filePath = $violation->getFileName();
+			$fileHtml = "<a href='file://$filePath' target='_blank'>" . self::highlightFile($filePath) . "</a>";
+
+			// Create an external link to rule's help, if there's any provided.
 			$linkHtml = null;
 			if ($url = $violation->getRule()->getExternalInfoUrl()) {
-				$linkHtml = "<a class='problem-link' href='{$url}' target='_blank'>(info)</a>";
+				$linkHtml = "<a class='info-lnk' href='{$url}' target='_blank'>(info)</a>";
 			}
 
+			// HTML snippet handling the toggle to display the file's code.
+			$showCodeAnchor = "
+				<a class='info-lnk blck' href='#' onclick='toggle(\"$htmlId-code\"); return false;'>
+					Show code &#x25BC;
+				</a>";
+
 			$html = "
-				<div class='problem'>
+				<section class='prb' id='$htmlId'>
 					<header>
-						<div class='problem-index'>{$index}</div>
-						<div class='problem-info'>{$infoHtml} {$linkHtml}</div>
+						<h3>
+							<a href='#$htmlId' class='indx'>#{$index}</a>
+							{$descHtml} {$linkHtml}
+						</h3>
 					</header>
-					<div class='problem-file'><b>File:</b> {$fileHtml}</div>
-					<div class='excerpt'>{$excerptHtml}</div>
-				</div>
-			";
+					<div class='file-pth'><b>File:</b> {$fileHtml} {$showCodeAnchor}</div>
+					<ul class='code hidden' id='$htmlId-code'>%s</ul>
+				</section>";
 
 			// Remove unnecessary tab/space characters at the line beginnings.
-			$w->write(preg_replace('#^\s+#mu', null, $html));
+			$html = self::reduceWhitespace($html);
+			$w->write(sprintf($html, $excerptHtml));
 
 		}
 
-		$this->glomProcessingErrors($report);
 	}
 
 	/**
@@ -292,47 +439,14 @@ class PrettyHTMLRenderer extends AbstractRenderer
 	public function end()
 	{
 		$writer = $this->getWriter();
-		$writer->write('</body></html>');
-	}
-
-	/**
-	 * This method will render a html table with occurred processing errors.
-	 *
-	 * @param \PHPMD\Report $report
-	 * @return void
-	 * @since 1.2.1
-	 */
-	private function glomProcessingErrors(Report $report)
-	{
-		if (false === $report->hasErrors()) {
-			return;
-		}
-
-		$writer = $this->getWriter();
-
-		$writer->write('<hr />');
-		$writer->write('<center><h3>Processing errors</h3></center>');
-		$writer->write('<table align="center" cellspacing="0" cellpadding="3">');
-		$writer->write('<tr><th>File</th><th>Problem</th></tr>');
-
-		$index = 0;
-		foreach ($report->getErrors() as $error) {
-			$writer->write('<tr');
-			if (++$index % 2 === 1) {
-				$writer->write(' bgcolor="lightgrey"');
-			}
-			$writer->write('>');
-			$writer->write('<td>' . $error->getFile() . '</td>');
-			$writer->write('<td>' . htmlentities($error->getMessage()) . '</td>');
-			$writer->write('</tr>' . PHP_EOL);
-		}
-
-		$writer->write('</table>');
+		$writer->write('</div></body></html>');
 	}
 
 	/**
 	 * Return array of lines from a specified file:line, optionally with extra lines around
 	 * for additional cognitive context.
+	 *
+	 * @return array
 	 */
 	protected static function getLineExcerpt($file, $lineNumber, $extra = 0)
 	{
@@ -355,6 +469,12 @@ class PrettyHTMLRenderer extends AbstractRenderer
 
 	}
 
+	/**
+	 * Take a rule description text and try to decorate/stylize parts of it with HTML.
+	 * Based on self::INFO_HIGHLIGHTING config.
+	 *
+	 * @return string
+	 */
 	protected static function colorize($message)
 	{
 		// Compile final regex, if not done already.
@@ -376,20 +496,126 @@ class PrettyHTMLRenderer extends AbstractRenderer
 			$definition = array_keys(array_intersect_key(self::INFO_HIGHLIGHTING, array_filter($x)));
 			$definition = reset($definition);
 
-			return "<span class='highlight-info {$definition}'>{$x[0]}</span>";
+			return "<span class='hlt-info {$definition}'>{$x[0]}</span>";
 
 		}, $message);
 
 	}
 
+	/**
+	 * Take a file path and return a bit of HTML where the basename is wrapped in styled <span>.
+	 *
+	 * @return string
+	 */
 	protected static function highlightFile($path)
 	{
-
 		$file = substr(strrchr($path, "/"), 1);
 		$dir = str_replace($file, null, $path);
 
 		return $dir . "<span class='path-basename'>" . $file . '</span>';
 
+	}
+
+	/**
+	 * Render a pretty informational table and send the HTML to the writer.
+	 *
+	 * @return void
+	 */
+	protected function writeTable($title, $itemsTitle, $items)
+	{
+		if (!$items) {
+			return;
+		}
+
+		$writer = $this->getWriter();
+		$rows = null;
+
+		// We will need to calculate percentages and whatnot.
+		$max = max($items);
+		$sum = array_sum($items);
+
+		foreach ($items as $name => $count) {
+
+			// Calculate chart/bar's percentage width relative to the highest occuring item.
+			$width = $max !== 0 ? $count / $max * 100 : 0; // Avoid division by zero.
+
+			$bar = sprintf(
+				'<div class="t-bar" style="width: %d%%; opacity: %.2f"></div>',
+				$width,
+				min(0.2 + $width / 100, 1) // Minimum opacity for the bar is 0.2.
+			);
+
+			$pct = $sum !== 0 ? sprintf('%.1f', $count / $sum * 100) : '-'; // Avoid division by zero.
+			$rows .= "<tr>
+				<td class='t-cnt'>$count</td>
+				<td class='t-pct'>$pct %</td>
+				<th class='t-n'>{$name}{$bar}</th>
+			</tr>";
+
+		}
+
+		$header = "<thead><tr><th>Count</th><th>%</th><th>$itemsTitle</th></tr></thead>";
+		$html = "<section><table><caption>$title</caption>{$header}{$rows}</table><section>";
+		$writer->write(self::reduceWhitespace($html));
+
+	}
+
+	/**
+	 * Go through passed violations and count occurences based on pre-specified conditions.
+	 *
+	 * @return
+	 */
+	protected static function sumUpViolations($violations)
+	{
+		$result = [
+			self::CATEGORY_PRIORITY => [],
+			self::CATEGORY_NAMESPACE => [],
+			self::CATEGORY_RULESET => [],
+			self::CATEGORY_RULE => [],
+		];
+
+		foreach ($violations as $v) {
+
+			// We use "ref" reference to make things somewhat easier to read.
+			// Also, using a reference to non-existing array index doesn't throw a notice.
+
+			if ($ns = $v->getNamespaceName()) {
+				$ref = &$result[self::CATEGORY_NAMESPACE][$ns];
+				$ref = isset($ref) ? $ref + 1 : 1;
+			}
+
+			$rule = $v->getRule();
+
+			// Friendly priority -> Add a describing word to "just number".
+			$friendlyPriority = self::PRIORITY_TITLES[$rule->getPriority()];
+			$ref = &$result[self::CATEGORY_PRIORITY][$friendlyPriority];
+			$ref = isset($ref) ? $ref + 1 : 1;
+
+			$ref = &$result[self::CATEGORY_RULESET][$rule->getRuleSetName()];
+			$ref = isset($ref) ? $ref + 1 : 1;
+
+			$ref = &$result[self::CATEGORY_RULE][$rule->getName()];
+			$ref = isset($ref) ? $ref + 1 : 1;
+
+		}
+
+		// Sort numbers in each category from high to low.
+		foreach ($result as &$inner) {
+			arsort($inner);
+		}
+
+		return $result;
+
+	}
+
+	/**
+	 * Reduces two and more whitespaces in a row to a single whitespace to conserve space.
+	 *
+	 * @return string
+	 */
+	protected static function reduceWhitespace(string $input, $eol = true)
+	{
+		return preg_replace("#\s+#", " ", $input) . ($eol ? PHP_EOL : null);
 	}
 
 }
