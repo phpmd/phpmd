f13754d7994003cee4ad3883dbbd07332e638891
Smuuf <premavansmuuf@gmail.com>
2019-07-04 14:22:26 +0200

    Cast a possible "false" value from SplFileObject::current() into string.

--- src/main/php/PHPMD/Renderer/HTMLRenderer.php
+++ src/main/php/PHPMD/Renderer/HTMLRenderer.php
@@ -467,7 +467,7 @@ class HTMLRenderer extends AbstractRenderer
 		if (!$file->eof()) {
 			$file->seek($line);
 			for ($i = 0; $i <= ($extra * 2); $i++) {
-				$result[++$line] = trim($file->current(), "\n");
+				$result[++$line] = trim((string) $file->current(), "\n");
 				$file->next();
 			}
 		}
