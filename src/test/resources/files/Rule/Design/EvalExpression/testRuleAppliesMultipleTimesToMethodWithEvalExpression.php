2adedb483aa9aea8cd7ee4318fb0b0f242ab42df
Marc Würth <ravage@bluewin.ch>
2020-05-09 00:11:24 +0200

    Use elseif instead of nested else & if
    https://github.com/phpmd/phpmd/pull/773/#pullrequestreview-408027674

--- src/test/resources/files/Rule/Design/EvalExpression/testRuleAppliesMultipleTimesToMethodWithEvalExpression.php
+++ src/test/resources/files/Rule/Design/EvalExpression/testRuleAppliesMultipleTimesToMethodWithEvalExpression.php
@@ -21,10 +21,8 @@ class testRuleAppliesMultipleTimesToMethodWithEvalExpression
     {
         if (true) {
             eval('$a = 17;');
-        } else {
-            if (time() % 42 === 0) {
-                eval('$a = 23;');
-            }
+        } elseif (time() % 42 === 0) {
+            eval('$a = 23;');
         }
         eval('$a = 42;');
     }
