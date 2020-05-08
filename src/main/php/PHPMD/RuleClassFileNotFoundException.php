fb048a501fdbdfbe9a2158ac3ae24e2be9c5149f
Marc Würth <ravage@bluewin.ch>
2020-05-09 00:28:14 +0200

    Add class aliases for moved exceptions

---
+++ src/main/php/PHPMD/RuleClassFileNotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of PHP Mess Detector.
+ *
+ * Copyright (c) Manuel Pichler <mapi@phpmd.org>.
+ * All rights reserved.
+ *
+ * Licensed under BSD License
+ * For full copyright and license information, please see the LICENSE file.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author Manuel Pichler <mapi@phpmd.org>
+ * @copyright Manuel Pichler. All rights reserved.
+ * @license https://opensource.org/licenses/bsd-license.php BSD License
+ * @link http://phpmd.org/
+ */
+
+namespace PHPMD;
+
+/**
+ * @deprecated 3.0.0 Use PHPMD\Exception\RuleClassFileNotFoundException instead.
+ */
+class_alias('PHPMD\Exception\RuleClassFileNotFoundException', 'PHPMD\RuleClassFileNotFoundException');
