e924069739826cdbc496a4599bf0e5039937bbbf
Marc Würth <ravage@bluewin.ch>
2020-05-11 19:19:56 +0200

    Use elseif instead of else + nested if
    Co-authored-by: Kyle <kylekatarnls@users.noreply.github.com>

--- src/test/resources/files/Rule/Design/EvalExpression/testRuleAppliesMultipleTimesToFunctionWithEvalExpression.php
+++ src/test/resources/files/Rule/Design/EvalExpression/testRuleAppliesMultipleTimesToFunctionWithEvalExpression.php
@@ -19,10 +19,8 @@ function testRuleAppliesMultipleTimesToFunctionWithEvalExpression()
 {
     if (true) {
         eval('$a = 17;');
-    } else {
-        if (time() % 42 === 0) {
-            eval('$a = 23;');
-        }
+    } elseif (time() % 42 === 0) {
+        eval('$a = 23;');
     }
     eval('$a = 42;');
 }
