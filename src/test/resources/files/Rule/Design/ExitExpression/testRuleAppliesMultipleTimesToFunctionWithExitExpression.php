a42fc207f0c171119728356bad507834343d134d
Marc Würth <ravage@bluewin.ch>
2020-05-11 19:20:11 +0200

    Use elseif instead of else + nested if
    Co-authored-by: Kyle <kylekatarnls@users.noreply.github.com>

--- src/test/resources/files/Rule/Design/ExitExpression/testRuleAppliesMultipleTimesToFunctionWithExitExpression.php
+++ src/test/resources/files/Rule/Design/ExitExpression/testRuleAppliesMultipleTimesToFunctionWithExitExpression.php
@@ -19,10 +19,8 @@ function testRuleAppliesMultipleTimesToFunctionWithExitExpression()
 {
     if (true) {
         exit(0);
-    } else {
-        if (time() % 42 === 0) {
-            exit(1);
-        }
+    } elseif (time() % 42 === 0) {
+        exit(1);
     }
     exit(2);
 }
