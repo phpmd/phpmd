ad70a1ae064b56eb82c4a8c9e6cee9c5d937e395
Kyle <kylekatarnls@users.noreply.github.com>
2020-07-23 10:16:32 +0200

    Fix PHP 5.3 compatibility for $parents

--- src/main/php/PHPMD/Rule/Naming/ShortVariable.php
+++ src/main/php/PHPMD/Rule/Naming/ShortVariable.php
@@ -208,9 +208,7 @@ class ShortVariable extends AbstractRule implements ClassAware, MethodAware, Fun
      * @param \PHPMD\AbstractNode $node
      * @return boolean
      */
-    protected function getParentsOfType(AbstractNode $node, $type)
-    {
-        $parents = [];
+        $parents = array();
 
         $parent = $node->getParent();
 
