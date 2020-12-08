cb15e4e9dbe78360512eb4a0d31d13c57709b214
Ulrich Tebin (EXT) <teul4973@eso.local>
2020-12-08 10:46:12 +0100

    corrected format

--- src/main/php/PHPMD/Renderer/DatatablesRenderer/DatatablesRenderer.js
+++ src/main/php/PHPMD/Renderer/DatatablesRenderer/DatatablesRenderer.js
@@ -1,41 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
     
-    function format ( d ) {
+    function format(d)
+    {
         return d[6];
     }
     
-    var table = $('#phpmdDatatable').DataTable({
-                                                   columnDefs: [
-                                                       {
-                                                           targets: 0,
-                                                           className:      'details-control',
-                                                           orderable:      false,
-                                                           data:           null,
-                                                           defaultContent: ''
-                                                       },
-                                                       {
-                                                           targets: [6],
-                                                           visible: false
-                                                       }
-                                                   ],
-                                                   pageLength: 100
-                                               });
+    var table = $('#phpmdDatatable').DataTable(
+        {
+            columnDefs: [
+                {
+                    targets       : 0,
+                    className     : 'details-control',
+                    orderable     : false,
+                    data          : null,
+                    defaultContent: ''
+                },
+                {
+                    targets: [6],
+                    visible: false
+                }
+            ],
+            pageLength: 100
+        }
+    );
     
     $('#phpmdDatatable tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row( tr );
+        var tr  = $(this).closest('tr');
+        var row = table.row(tr);
         
-        if ( row.child.isShown() ) {
+        if(row.child.isShown())
+        {
             // This row is already open - close it
             row.child.hide();
             tr.removeClass('shown');
         }
-        else {
+        else
+        {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child(format(row.data())).show();
             tr.addClass('shown');
         }
-    } );
+    });
     
     
-} );
+});
