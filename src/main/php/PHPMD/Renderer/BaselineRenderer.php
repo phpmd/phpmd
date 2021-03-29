312f47b341f33764c0331a95e2453914ff27b8e2
Frank Dekker <fdekker@gmail.com>
2021-03-30 00:09:27 +0200

    Added BaselineFileFinder to find the baseline file.

--- src/main/php/PHPMD/Renderer/BaselineRenderer.php
+++ src/main/php/PHPMD/Renderer/BaselineRenderer.php
@@ -4,6 +4,7 @@ namespace PHPMD\Renderer;
 
 use PHPMD\AbstractRenderer;
 use PHPMD\Report;
+use PHPMD\Utility\Paths;
 
 class BaselineRenderer extends AbstractRenderer
 {
@@ -15,7 +16,7 @@ class BaselineRenderer extends AbstractRenderer
      */
     public function __construct($baseDir)
     {
-        $this->baseDir = rtrim(str_replace("\\", "/", $baseDir), '/') . '/';
+        $this->baseDir = $baseDir;
     }
 
     public function renderReport(Report $report)
@@ -31,30 +32,11 @@ class BaselineRenderer extends AbstractRenderer
             $xmlTag = sprintf(
                 '  <violation rule="%s" file="%s"/>' . PHP_EOL,
                 get_class($rule),
-                $this->getRelativePath($filepath)
+                Paths::getRelativePath($this->baseDir, $filepath)
             );
             $writer->write($xmlTag);
         }
 
         $writer->write('</phpmd-baseline>' . PHP_EOL);
-    }
-
-    /**
-     * Transform the given absolute path to the relative path within the input path
-     *
-     * @param string $filepath
-     * @return string
-     */
-    private function getRelativePath($filepath)
-    {
-        // transform all slashes to forward slashes
-        $filepath = str_replace("\\", "/", $filepath);
-
-        // subtract base dir from filepath if there's a match
-        if (stripos($filepath, $this->baseDir) === 0) {
-            $filepath = substr($filepath, strlen($this->baseDir));
-        }
-
-        return $filepath;
     }
 }
--- src/main/php/PHPMD/Utility/Paths.php
+++ src/main/php/PHPMD/Utility/Paths.php
@@ -17,4 +17,25 @@ class Paths
         $pathB = ltrim(str_replace('\\', '/', $pathB), '/');
         return $pathA . '/' . $pathB;
     }
+
+    /**
+     * Transform the given absolute path to the relative path within based on the base path.
+     *
+     * @param string $basePath
+     * @param string $filePath
+     * @return string
+     */
+    public static function getRelativePath($basePath, $filePath)
+    {
+        // normalize slashes and ensure base path ends with slash
+        $basePath = rtrim(str_replace('\\', '/', $basePath), '/') . '/';
+        $filePath = str_replace('\\', '/', $filePath);
+
+        // subtract base dir from filepath if there's a match
+        if (stripos($filePath, $basePath) === 0) {
+            $filePath = substr($filePath, strlen($basePath));
+        }
+
+        return $filePath;
+    }
 }
