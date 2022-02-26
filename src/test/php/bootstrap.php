0afd76149325a3f2edf8835e331c89e0ddd965a8
kylekatarnls <kylekatarnls@gmail.com>
2022-02-26 23:35:16 +0100

    Fix dependencies files for PHP 8.1 compatibility

--- src/test/php/bootstrap.php
+++ src/test/php/bootstrap.php
@@ -15,55 +15,6 @@
  * @link http://phpmd.org/
  */
 
-$replacements = array(
-    /**
-     * Patch phpunit/phpunit-mock-objects Generator.php file to not create double nullable tokens: `??`
-     */
-    __DIR__ . '/../../../vendor/phpunit/phpunit-mock-objects/src/Framework/MockObject/Generator.php' => array(
-        array(
-            "if (version_compare(PHP_VERSION, '7.1', '>=') && " .
-                "\$parameter->allowsNull() && !\$parameter->isVariadic()) {",
-            "if (version_compare(PHP_VERSION, '7.1', '>=') && version_compare(PHP_VERSION, '8.0', '<') && " .
-                "\$parameter->allowsNull() && !\$parameter->isVariadic()) {",
-        ),
-    ),
-    /**
-     * Fix phpunit/phpunit to not trigger warning on `final private function`
-     */
-    __DIR__ . '/../../../vendor/phpunit/phpunit/src/Util/Configuration.php' => array(
-        array(
-            'final private function',
-            'private function',
-        ),
-    ),
-);
-
-foreach ($replacements as $file => $patterns) {
-    echo "$file: ";
-
-    if (!file_exists($file)) {
-        echo "File not found.\n";
-
-        continue;
-    }
-
-    foreach ($patterns as $replacement) {
-        list($from, $to) = $replacement;
-
-        $contents = @file_get_contents($file) ?: '';
-        $newContents = str_replace($from, $to, $contents);
-
-        if ($newContents !== $contents) {
-            file_put_contents($file, $newContents);
-            echo "Content changed.\n";
-
-            continue;
-        }
-
-        echo "Replace pattern not found.\n";
-    }
-}
-
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 spl_autoload_register(
