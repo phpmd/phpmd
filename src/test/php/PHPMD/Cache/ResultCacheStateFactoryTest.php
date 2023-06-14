f59c603df914635b44cdf748942e122ab96cf707
Frank Dekker <fdekker@gmail.com>
2023-06-14 23:49:20 +0200

    Add coverage ResultCacheStateFactory

---
+++ src/test/php/PHPMD/Cache/ResultCacheStateFactoryTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace PHPMD\Cache;
+
+use PHPMD\AbstractTest;
+use PHPMD\Cache\Model\ResultCacheKey;
+
+/**
+ * @coversDefaultClass \PHPMD\Cache\ResultCacheStateFactory
+ * @covers ::__construct
+ */
+class ResultCacheStateFactoryTest extends AbstractTest
+{
+    /** @var ResultCacheStateFactory */
+    private $factory;
+
+    protected function setUp()
+    {
+        $this->factory = new ResultCacheStateFactory();
+    }
+
+    /**
+     * @covers ::fromFile
+     */
+    public function testFromFileNonExisting()
+    {
+        $state = $this->factory->fromFile('foobar');
+        static::assertNull($state);
+    }
+
+    /**
+     * @covers ::fromFile
+     * @covers ::createCacheKey
+     */
+    public function testFromFileEmptyCache()
+    {
+        $state = $this->factory->fromFile(static::createResourceUriForTest('.empty-cache.php'));
+        static::assertNull($state);
+    }
+
+    /**
+     * @covers ::fromFile
+     * @covers ::createCacheKey
+     */
+    public function testFromFileIncompleteCacheKey()
+    {
+        $state = $this->factory->fromFile(static::createResourceUriForTest('.incomplete-cache.php'));
+        static::assertNull($state);
+    }
+
+    /**
+     * @covers ::fromFile
+     * @covers ::createCacheKey
+     */
+    public function testFromFileFullCache()
+    {
+        $state = $this->factory->fromFile(static::createResourceUriForTest('.result-cache.php'));
+
+        // assert cache key
+        $expectedKey = new ResultCacheKey(false, array('rule' => 'hash'), array('composer.json' => 'hash1', 'composer.lock' => 'hash2'), 70000);
+        $cacheKey    = $state->getCacheKey();
+        static::assertEquals($expectedKey, $cacheKey);
+
+        // assert file state
+        static::assertFalse($state->isFileModified('file1', 'file1-hash'));
+        static::assertTrue($state->isFileModified('file2', 'file1-hash'));
+        static::assertSame(array('violations'), $state->getViolations('file2'));
+    }
+}
---
+++ src/test/resources/files/Cache/ResultCacheStateFactory/.result-cache.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+    'key'   =>
+        array(
+            'strict'     => false,
+            'rules'      =>
+                array(
+                    'rule' => 'hash',
+                ),
+            'composer'   =>
+                array(
+                    'composer.json' => 'hash1',
+                    'composer.lock' => 'hash2',
+                ),
+            'phpVersion' => 70000,
+        ),
+    'state' =>
+        array(
+            'files' =>
+                array(
+                    'file1' =>
+                        array(
+                            'hash'       => 'file1-hash',
+                            'violations' => array(),
+                        ),
+                    'file2' =>
+                        array(
+                            'hash'       => 'file2-hash',
+                            'violations' => array('violations'),
+                        ),
+                ),
+        ),
+);
