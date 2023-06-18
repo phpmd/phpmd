be344eff5a3c6aa1d661b3bb376f34ba26de5c30
Frank Dekker <fdekker@gmail.com>
2023-06-18 14:50:55 +0200

    Add coverage ResultCacheState

---
+++ src/test/php/PHPMD/Cache/Model/ResultCacheStateTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace PHPMD\Cache\Model;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \PHPMD\Cache\Model\ResultCacheState
+ * @covers ::__construct
+ */
+class ResultCacheStateTest extends TestCase
+{
+
+    /**
+     * @covers ::addRuleViolation
+     */
+    public function testAddRuleViolation()
+    {
+
+    }
+
+    /**
+     * @covers ::getCacheKey
+     */
+    public function testGetCacheKey()
+    {
+
+    }
+
+    /**
+     * @covers ::getRuleViolations
+     */
+    public function testGetRuleViolations()
+    {
+
+    }
+
+    /**
+     * @covers ::getViolations
+     */
+    public function testGetViolations()
+    {
+
+    }
+
+    /**
+     * @covers ::isFileModified
+     */
+    public function testIsFileModified()
+    {
+
+    }
+
+    /**
+     * @covers ::setViolations
+     */
+    public function testSetViolations()
+    {
+
+    }
+
+    /**
+     * @covers ::toArray
+     */
+    public function testToArray()
+    {
+
+    }
+
+    /**
+     * @covers ::setFileState
+     */
+    public function testSetFileState()
+    {
+
+    }
+}
