c8129b3e078ccb1534fbb98de67e22d1dca84587
Frank Dekker <fdekker@gmail.com>
2023-07-02 11:59:34 +0200

    Fix codestyle

--- src/main/php/PHPMD/Cache/ResultCacheEngineFactory.php
+++ src/main/php/PHPMD/Cache/ResultCacheEngineFactory.php
@@ -16,9 +16,10 @@ class ResultCacheEngineFactory
     private $cacheStateFactory;
 
     public function __construct(
-        OutputInterface $output,
-        ResultCacheKeyFactory $cacheKeyFactory,
-        ResultCacheStateFactory $cacheStateFactory)
+        OutputInterface         $output,
+        ResultCacheKeyFactory   $cacheKeyFactory,
+        ResultCacheStateFactory $cacheStateFactory
+    )
     {
         $this->output            = $output;
         $this->cacheKeyFactory   = $cacheKeyFactory;
