020b8d51e5585a9035835268ea480ddcd300ac9b
Ravage84 <ravage@bluewin.ch>
2024-05-05 01:39:23 +0200

    Apply function_declaration rule

--- src/main/php/PHPMD/Utility/Strings.php
+++ src/main/php/PHPMD/Utility/Strings.php
@@ -88,10 +88,10 @@ class Strings
 
         return array_filter(
             array_map(
-                static fn ($value) => Strings::trim($value, $trim),
+                static fn($value) => Strings::trim($value, $trim),
                 explode($separator, $listAsString)
             ),
-            static fn ($value) => $value !== ''
+            static fn($value) => $value !== ''
         );
     }
 
