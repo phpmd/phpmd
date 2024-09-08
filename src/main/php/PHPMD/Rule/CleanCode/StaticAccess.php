e45e096d7559894e1fb105470ce3b9becf25e63f
Norman Huth <norman@huth.it>
2024-09-08 13:24:05 +0200

    feat: add wildcard option for static access

--- src/main/php/PHPMD/Rule/CleanCode/StaticAccess.php
+++ src/main/php/PHPMD/Rule/CleanCode/StaticAccess.php
@@ -66,7 +66,21 @@ class StaticAccess extends AbstractRule implements MethodAware, FunctionAware
 
     protected function isExcludedFromAnalysis($className, $exceptions)
     {
-        return in_array(trim($className, " \t\n\r\0\x0B\\"), $exceptions);
+        $className = trim($className, " \t\n\r\0\x0B\\");
+
+        if (in_array($className, $exceptions)) {
+            return true;
+        }
+
+        $wildcardExceptions = array_filter($exceptions, fn ($exception) => strpos($exception, '*') !== false);
+        foreach ($wildcardExceptions as $wildcardException) {
+            $wildcardException = str_replace(['*', '\\'], ['.*', '\\\\'], $wildcardException);
+            if (preg_match('/' . $wildcardException . '/', $className)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     protected function isStaticMethodCall(AbstractNode $methodCall)
